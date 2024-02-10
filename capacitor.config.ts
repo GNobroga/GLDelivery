e45c1aee0757239ebf995a9c883a7436a1351a95
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-09 23:15:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GLFood',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
