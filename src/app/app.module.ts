341a02c3af3fbfd7b8ebbc0cf17613886fd709e5
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-12 19:12:11 -0300

    [ADD] Folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideAnimations()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
