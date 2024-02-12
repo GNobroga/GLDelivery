78074e469724704a8a585377020b995e24eafc55
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-12 10:23:07 -0300

    [INFO] Página de Welcome pronta

--- src/app/pages/welcome/welcome-routing.module.ts
+++ src/app/pages/welcome/welcome-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import WelcomeComponent from './welcome.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: '',
+    component: WelcomeComponent,
     children: [
       {
         path: 'page1',
@@ -22,11 +26,11 @@ const routes: Routes = [
       },
       {
         path: 'page3',
-        component: Page2Component,
+        component: Page3Component,
       },
       {
         path: 'page4',
-        component: Page2Component,
+        component: Page4Component,
       },
     ],
   }
