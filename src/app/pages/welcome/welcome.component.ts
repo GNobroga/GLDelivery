78cc798739f16a33a3428557a337685456214248
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-12 10:32:53 -0300

    [INFO] Correção na rota welcome

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
+import { NavController } from "@ionic/angular";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export default class WelcomeComponent {}
+export default class WelcomeComponent {
+
+  public pages: string[] = ['page1', 'page2', 'page3', 'page4'];
+
+  public currentPage = signal(0);
+
+  #navController = inject(NavController);
+
+  public next(): void {
+    this.currentPage.set(this.currentPage() + 1);
+    const page = this.pages[this.currentPage() % this.pages.length];
+    this.#navController.navigateForward([`/welcome/${page}`]);
+  }
+
+}
