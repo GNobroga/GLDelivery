ab7548cf9320d9fba9271fe714650f99508c8888
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-12 14:24:37 -0300

    [ADD] Resgister Page

--- src/app/pages/account/account-options/account-options.component.ts
+++ src/app/pages/account/account-options/account-options.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-account-options',
   templateUrl: './account-options.component.html',
   styleUrls: ['./account-options.component.scss'],
 })
-export class AccountOptionsComponent  {
+export class AccountOptionsComponent implements DoCheck {
+
+  @Input()
+  public mode: 'register' | 'sign' = 'sign';
+
+  public isRegister = signal(false);
 
   constructor() { }
+
+  public ngDoCheck(): void {
+    if (this.mode === 'register') {
+      this.isRegister.set(true);
+    }
+  }
 
 
 }
