7a364fd1788738137bd1eb5ebc693f70596a0cb7
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2024-02-14 21:35:34 -0300

    [ADD] Slide auto move

--- src/app/pages/tabs/components/highlights/highlights.component.ts
+++ src/app/pages/tabs/components/highlights/highlights.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild, inject } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+import { Subscription, interval, of,  switchMap, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-highlights',
   templateUrl: './highlights.component.html',
   styleUrls: ['./highlights.component.scss'],
 })
-export class HighlightsComponent {
+export class HighlightsComponent implements AfterViewInit, OnDestroy {
 
   @Input()
   public direction: 'row' | 'column' = 'row';
@@ -13,7 +15,50 @@ export class HighlightsComponent {
   @Input()
   public favoriteMode = false;
 
-  constructor() { }
+  @ViewChild('list')
+  public highlights!: ElementRef<HTMLUListElement>;
+
+  public subscriptions: Subscription[] = [];
+
+  #animationController = inject(AnimationController);
+
+  constructor() {
+
+   }
+
+  public ngAfterViewInit(): void {
+    if (!this.highlights.nativeElement.hasChildNodes() || this.favoriteMode) {
+      return;
+    }
+
+    const CHILD_SIZE = 260;
+    const scrollLimitSize = (this.highlights.nativeElement.childNodes.length - 1) * CHILD_SIZE;
+
+    let backSlide = false;
+
+    const subscription = interval(1000)
+      .pipe(
+        throttleTime(500),
+        switchMap(() => of(this.highlights.nativeElement)))
+      .subscribe(e => {
+
+        const scrollSize = e.scrollLeft;
+
+        if (scrollSize >= scrollLimitSize || backSlide && scrollSize !== 0) {
+          e.scrollTo({ left: e.scrollLeft - CHILD_SIZE, behavior: 'smooth' });
+          backSlide = true;
+        } else {
+          e.scrollTo({ left: e.scrollLeft + CHILD_SIZE, behavior: 'smooth' });
+          backSlide = false;
+        }
+      });
+
+      this.subscriptions.push(subscription);
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach(x => x.unsubscribe());
+  }
 
 
 }
